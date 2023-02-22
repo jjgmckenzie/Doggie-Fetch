2097fa0413ba46f7d1cd0e1b72a4a80aa3f07ee5
James McKenzie <james@jjgmckenzie.ca>
2023-02-21 18:38:02 -0800

    FetchDoggy -> uses Tailwind, because MUI wasn't working with this version of NextJS. Adds Favicon, removes create nextjs app bloat

---
+++ src/app/header.tsx
@@ -0,0 +1,11 @@
+
+export default function Header(){
+    return (
+        <div className="w-full shadow-lg bg-slate-50">
+            <header className="max-w-7xl mx-auto flex px-2 ">
+                <img src="/favicon.svg" className="h-20 w-20 pr-4"/>
+                <h1 className="my-auto font-bold text-4xl drop-shadow-xl text-slate-900">Fetch Doggy</h1>
+            </header>
+        </div>
+    )
+}
