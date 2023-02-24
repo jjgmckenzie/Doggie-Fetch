fd602065a819c3a5dbfa4a0e1e542d169d90980a
James McKenzie <james@jjgmckenzie.ca>
2023-02-24 00:09:24 -0800

    Begins to set up uploading files to website

--- src/app/DogControlPanel.tsx
+++ src/app/DogControlPanel.tsx
@@ -1,13 +1,45 @@
 `use client`
-import {Dispatch, SetStateAction} from "react";
+import {Dispatch, SetStateAction, useCallback} from "react";
 
 
 interface Props{
-    setFilterPoppedUp:Dispatch<SetStateAction<boolean>>
+    optionsPoppedUp:boolean
+    uploadPoppedUp:boolean
+    setOptionsPoppedUp:Dispatch<SetStateAction<boolean>>
+    setUploadPoppedUp:Dispatch<SetStateAction<boolean>>
 }
 
+enum panelSelected{
+    options,
+    upload
+}
+
+
 export default function DogControlPanel (props:Props) {
+
+    const togglePanelShown = useCallback((toggledBy:panelSelected)=>{
+        if(props.optionsPoppedUp && toggledBy == panelSelected.options){
+            props.setOptionsPoppedUp(false)
+        }
+        else if(props.uploadPoppedUp && toggledBy == panelSelected.upload){
+            props.setUploadPoppedUp(false)
+        }
+        else{
+            if(toggledBy == panelSelected.upload){
+                props.setOptionsPoppedUp(false)
+                props.setUploadPoppedUp(true)
+            }
+            if(toggledBy == panelSelected.options){
+                props.setOptionsPoppedUp(true)
+                props.setUploadPoppedUp(false)
+            }
+        }
+    },[props])
+
     return (
-            <button className="ctrl-panel text-2xl p-2 min-w-fit my-auto" onClick={()=>{props.setFilterPoppedUp(prevState => !prevState)}}>Options</button>
+        <div className="w-16 mr-4">
+            <button className="ctrl-panel px-1 min-w-fit" onClick={()=>{togglePanelShown(panelSelected.options)}}>Options</button><br/>
+            <button className="ctrl-panel mt-3 px-1 min-w-fit" onClick={()=>{togglePanelShown(panelSelected.upload)}}>Upload</button>
+        </div>
     )
 }
--- src/app/DogController.tsx
+++ src/app/DogController.tsx
@@ -17,10 +17,11 @@ interface Props{
     direction:string
 }
 export default function DogController(props:Props){
-    const [filterPoppedUp, setFilterPoppedUp] = useState(false)
+    const [optionsPoppedUp, setOptionsPoppedUp] = useState(false)
+    const [uploadPoppedUp, setUploadPoppedUp] = useState(false)
 
     const FilterPanel = useCallback(()=>{
-        if(filterPoppedUp) {
+        if(optionsPoppedUp) {
             return (
                 <div className="shadow-xl rounded-lg max-w-2xl w-[80vw] bg-white mb-2 p-4 mx-auto">
                     <DogDropDown {...props}/>
@@ -33,18 +34,32 @@ export default function DogController(props:Props){
                 </div>
             )
         }
+        if(uploadPoppedUp) {
+            return (
+                <div className="shadow-xl rounded-lg max-w-2xl w-[95vw] bg-white mb-2 px-4 pt-4 pb-2 mx-auto">
+                    <h1 className="mx-auto text-xl sm:text-3xl text-center">Add your pooch to the Internet&apos;s biggest collection of <strong>open source dog pictures!</strong></h1>
+                    <p className="my-2 px-1 leading-tight">The images available on this site are initially <i>fetched</i> from <a href="https://dog.ceo/api" className="text-blue-500 underline">Dog API</a>, who accept new members to their pack! You can upload your pictures here, and our bot will submit your good boy / girl to them on your behalf.</p>
+                    <ul className="text-sm list list-disc leading-4 pl-7 pr-1 mb-2">
+                        <li>Please ensure your photos are of a good quality and the dog is easily identifiable in the photo</li>
+                        <li>Please ensure your photo features one dog only (although additional dogs can be in the background)</li>
+                        <li>Photos must not include any human or any part of a human (GDPR)</li>
+                    </ul>
+                    <p className="px-1 text-xs sm:text-sm tracking-tight leading-3"><strong>Note: </strong>Fetch! uses AI vision to prevent misuse, which may flag your upload incorrectly. In this case, you can manually submit your photos as a <a className="text-blue-500 underline" href="https://github.com/jigsawpieces/dog-api-images#dog-api-images">GitHub pull request here.</a> </p>
+                </div>
+            )
+        }
         return (
             <>
             </>
         )
-    },[filterPoppedUp, props])
+    },[optionsPoppedUp, props, uploadPoppedUp])
 
     return(
         <div className="fixed bottom-0 right-0 left-0 w-min mx-auto z-10 mb-3 sm:mb-5">
             {FilterPanel()}
             <div className="bg-white bg-opacity-25 rounded-lg backdrop-blur-sm shadow p-3 flex w-min mx-auto">
-                <DogControlPanel setFilterPoppedUp={setFilterPoppedUp}/>
-                <div className="mt-auto w-60">
+                <DogControlPanel setOptionsPoppedUp={setOptionsPoppedUp} optionsPoppedUp={optionsPoppedUp} uploadPoppedUp={uploadPoppedUp} setUploadPoppedUp={setUploadPoppedUp}/>
+                <div className="mt-auto w-48">
                     <DirectionControl setDirection={props.setDirection}/>
                 </div>
             </div>
