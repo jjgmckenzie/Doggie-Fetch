47680d070f3cf8a37ca5ad346e9efe3f65365174
James McKenzie <james@jjgmckenzie.ca>
2023-03-04 13:54:13 -0800

    Dockerises, adds license.

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "log"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	log.Fatalln(DefaultRunner(":8080").run("dog-api-images"))
 }
